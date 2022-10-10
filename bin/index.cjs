c3f679f0d6710176ba2359ac366f028c63f429b3
holly <h.person.notrobot@gmail.com>
2022-10-10 16:39:51 +0100

    Moved around some code in index to change where the html lines are processed  - Removing duplicate not working fully

--- bin/index.cjs
+++ bin/index.cjs
@@ -4,45 +4,48 @@ const fs = require("fs");
 const request = require("request-promise")
 
 function matchRuleShort(str, rule) {
-    let escapeRegex = (str) => str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
-    return new RegExp("^" + rule.split("*").map(escapeRegex).join(".*") + "$").test(str);
+    let escapeRegex = (str) => str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1")
+    return new RegExp("^" + rule.split("*").map(escapeRegex).join(".*") + "$").test(str)
 }
 
 
 async function search_anime(search){
     let filter = "*<ahref=\"/category/*\"title=\"*\">"
-    let x = await request({
+    await request({
         url: "https://gogoanime.lu//search.html?keyword="+search
     }, function(error, response, body){
         let html = body.split('\n')
-        let lines = ""
+        let lines = []
         for (let x in html){
             html[x] = html[x].replace(/ /g,'').replace(/\t/g,'')
             if (matchRuleShort(html[x], filter)){
-                lines += html[x]+"\n"
+                lines.push(html[x])
             }
         }
-        fs.writeFileSync("./temp.txt", lines, function(err) {
+        let text = ""
+        for (let x in lines){
+            lines[x] = lines[x].replace("<ahref=\"/category/", "").replace("\"title=\"", "").replace("\">", "").replaceAll("-", " ")
+            lines[x] = lines[x].slice(0, Math.ceil(lines[x].length / 2)+1);
+            text += lines[x]+"\n"
+        }
+
+        fs.writeFileSync("./temp.txt", text, function(err) {
             if(err) {
                 return console.log(err);
             }
-            console.log("The file was saved!");
         });
     })
 
+
+
     let text = fs.readFileSync("./temp.txt").toString().split("\n");
-
-    for (x in text){
-        text[x] = text[x].replace("<ahref=\"/category/", "").replace("\"title=\"", "").replace("\">", "")
-        text[x] = text[x].slice(0, Math.ceil(text[x].length / 2));
-    }
     text.pop()
-
     return text
 }
 
 async function main(){
-    console.log(await search_anime("one"))
+    console.log("Animaes: ")
+    console.log(await search_anime("sword art online"))
 }
 
 main()
