83a71ec1e94a52ff6d24c780b6b0035e6f365cef
hank <h.person.notrobot@gmail.com>
2022-10-10 18:19:10 +0000

    Added download() function and sample downloaded mp4

--- bin/index.cjs
+++ bin/index.cjs
@@ -1,7 +1,8 @@
 #! /usr/bin/env node
 const args = process.argv.slice(2);
 const fs = require("fs");
-const request = require("request-promise")
+const request = require("request-promise");
+const http = require('http');
 
 function matchRuleShort(str, rule) {
     let escapeRegex = (str) => str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
@@ -29,21 +30,37 @@ async function search_anime(search){
             console.log("The file was saved!");
         });
     })
-
     let text = fs.readFileSync("./temp.txt").toString().split("\n");
-
     for (x in text){
-        text[x] = text[x].replace("<ahref=\"/category/", "").replace("\"title=\"", "").replace("\">", "")
-        text[x] = text[x].slice(0, Math.ceil(text[x].length / 2));
+        text[x] = text[x].slice(text[x].indexOf("/category/")+10);
+        text[x] = text[x].slice(0, text[x].indexOf("\"title=")).replaceAll("-", " ");
     }
     text.pop()
-
     return text
 }
+
+
+async function download(url, name){
+    let file = fs.createWriteStream("./../downloads/"+name)
+    http.get(url, function(response) {
+        response.pipe(file);
+        // after download completed close filestream
+        file.on("finish", () => {
+            file.close();
+            console.log(`Downloaded: ${name}`);
+        });
+    });
+}
+
+
+
+
 
 async function main(){
     console.log("Animaes: ")
     console.log(await search_anime("sword art online"))
+
+    await download("http://techslides.com/demos/sample-videos/small.mp4", "sample.mp4")
 }
 
 main()
