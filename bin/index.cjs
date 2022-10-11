964d4c0e5a4bf3bfcc9ad23cf8d8f5918f85031c
hank <h.person.notrobot@gmail.com>
2022-10-11 21:03:08 +0000

    Added video link finders. Added vlc controller. To be replaced. Finally working

--- bin/index.cjs
+++ bin/index.cjs
@@ -1,24 +1,19 @@
 #! /usr/bin/env node
-const args = process.argv.slice(2);
 const fs = require("fs");
 const http = require('http');
-const request = require("request")
+//const HttpsProxyAgent = require('https-proxy-agent');
+const VLC = require('vlc-simple-player')
+//const proxyAgent = new HttpsProxyAgent("68.183.230.116:3951");
+const prompt = require("simple-input");
+async function input(message){
+    console.log(colors.Magenta,message)
+    return await prompt(">")
+}
 
-const HttpsProxyAgent = require('https-proxy-agent');
-const proxyAgent = new HttpsProxyAgent("68.183.230.116:3951");
 
-const agent="Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/100.0"
-const PID=0
-const scrape = "Query"
-const quality="best"
-const fzf = 0
-const auto_play=0
 const download_dir = "./../downloads/"
 const gogohd_url="https://gogohd.net/"
 const base_url="https://animixplay.to"
-const player_fn = "download"
-const input = require("simple-input");
-
 
 const colors = {
     Black: "\x1b[30m%s\x1b[0m",
@@ -32,16 +27,16 @@ const colors = {
 }
 
 
-
-async function curl(url){
-    fetch(url, {
-        "agent": proxyAgent,
+async function curl(url, method="GET"){
+    await fetch(url, {
+        //"agent": proxyAgent,
         "headers": {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/100.0'
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/100.0',
+            "X-Requested-With": "XMLHttpRequest"
         },
         "referrerPolicy": "origin",
         "body": null,
-        "method": "GET",
+        "method": method,
         "proxy": "68.183.230.116:3951"
     }).then(function (response) {
         return response.text();
@@ -50,7 +45,6 @@ async function curl(url){
             if(err) {
                 return console.log(err);
             }
-            console.log("The file was saved!");
         });
     }).catch(function (err) {
         console.warn(`Something went wrong connecting to ${url}.`, err);
@@ -88,8 +82,8 @@ async function search_anime(search){
 async function episode_list(anime_id){
     let html = (await curl(base_url+"/v1/"+anime_id)).split("\n")
     let lines = ""
-    for (x in html){
-        if(matchRuleShort(html[x], "*epslistplace*")){
+    for (let x in html){
+        if(matchRuleShort(html[x], "*<div id=\"epslistplace\"*")){
             lines = (html[x])
         }
     }
@@ -117,27 +111,16 @@ async function download(url, name){
 }
 
 
-function episode_selection(episodes){
+async function selection(options, prompt){
     let selection
-    while (true){
-        selection = 3
-        if (selection < episodes && selection > 1){
+    while (!(selection <= options && selection > 1)){
+        selection = (await input(prompt))
+        if (selection <= options && selection >= 1){
             break
         }
+        console.log(colors.Red,`Please input a valid option.`)
     }
     return selection
-}
-
-function anime_selection(list){
-    let selection
-    while (true){
-        selection = 3
-        if (selection <= list.length && selection > 1){
-            break
-        }
-        console.log("Please input a valid option.")
-    }
-    return list[selection]
 }
 
 
@@ -149,18 +132,64 @@ async function process_search(query){
         return 0
     }else{
         for (x in search_results){
-            console.log(`${parseInt(x)+1})${" ".repeat(((search_results.length).toString().length+1)-((parseInt(x)+1).toString().length))}${search_results[x].replaceAll("-", " ")}`)
+            console.log(colors.Cyan,`${parseInt(x)+1})${" ".repeat(((search_results.length).toString().length+1)-((parseInt(x)+1).toString().length))}${search_results[x].replaceAll("-", " ")}`)
         }
     }
-    let anime_id = anime_selection(search_results)
-    let episode = episode_selection(episode_list(anime_id))
-    return 0
+    let anime_id = search_results[await selection(search_results.length, "Please select an anime.")-1]
+    let episodes = await episode_list(anime_id)
+    let episode_number = await selection(episodes.length, `Please select an episode (1-${episodes.length}).`)
+    return {anime_id:anime_id,
+        episodes:episodes,
+        episode_number:episode_number
+    }
 }
 
-async function main(){
-    console.log(colors.Cyan, 'I am cyan')
 
-    await process_search("sword art online")
+async function get_video_link(episode_dpage){
+    let id = episode_dpage.replace("//gogohd.net/streaming.php?id=","")
+    id = id.slice(0, id.indexOf("="))
+
+    let html = await curl(`${gogohd_url}streaming.php?id=${id}`)
+    return await generate_link(1, html)
+}
+
+
+async function generate_link(provider, html){
+    switch (provider) {
+        case 1:
+            let provider_name = 'Xstreamcdn'
+            console.log(colors.Blue, `Fetching ${provider_name} links...`)
+            html = html.split("\n")
+            let fb_id = ""
+            for (x in html){
+                if (matchRuleShort(html[x], "*<li class=\"linkserver\" data-status=\"1\" data-video=\"https://fembed9hd.com/v/*")){
+                    fb_id = html[x].slice(html[x].indexOf("/v/")+3, html[x].indexOf("\">X"))
+                }
+            }
+            if (!fb_id){
+                console.log("Error, no fb_id found.")
+                return 0
+            }
+            //let refr = "https://fembed-hd.com/v/"+fb_id
+            let post = await curl("https://fembed-hd.com/api/source/"+fb_id, "POST")
+            post = post.slice(post.indexOf(",\"data\":[{\"file\":\"")+18, post.length)
+            post = post.slice(0, post.indexOf("\"")).replaceAll("\\/","/")
+            return post
+    }
+}
+
+
+
+
+
+async function main(){
+    let choice = await input("Search anime.")
+    let anime = await process_search(choice)
+    let link = await get_video_link(anime.episodes[anime.episode_number])
+    let player = new VLC(link)
+    this.process.on('close', (code) => {
+        console.log("exit")
+    })
 }
 
 main()
