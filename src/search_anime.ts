c97d0c2484716d2ffdf9bc081f6ba99df46bd124
Holly <h.person.notrobot@gmail.com>
2022-11-26 15:39:01 +0000

    Added search_anime.ts file

---
+++ src/search_anime.ts
@@ -0,0 +1,24 @@
+import {RegexParse} from "./regex";
+import {curl} from "./curl";
+
+async function search_anime(search: string){
+    let filter = "*<ahref=\"/category/*\"title=\"*\">"
+    let html: string[] = (await curl("https://gogoanime.dk//search.html?keyword="+search)).split("\n")
+    let lines: string[] = []
+    for (let x in html){
+        html[x] = html[x].replaceAll(/ /g,'').replaceAll(/\t/g,'')
+        if (RegexParse(html[x], filter)){
+            html[x] = html[x].slice(html[x].indexOf("/category/")+10);
+            html[x] = html[x].slice(0, html[x].indexOf("\"title="));
+            lines.push(html[x])
+        }
+    }
+    if (!lines[0]){
+        lines.pop()
+    }
+
+
+    return lines
+}
+
+export {search_anime}
