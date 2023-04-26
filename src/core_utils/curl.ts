2cb81ed8793f384d7eff5693bb49488868606f0c
holly <h.person.notrobot@gmail.com>
2023-04-26 15:12:14 +0100

    Made curl function, with optional additional https headers etc

--- src/core_utils/curl.ts
+++ src/core_utils/curl.ts
@@ -1,5 +1,52 @@
-async function curl(){
-    console.log("Hello from the curl function!")
+import https from "https";
+import querystring from "querystring";
+
+import {config} from "../config";
+
+const agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'; // Replace with your desired User-Agent string
+
+
+interface CurlOptions {
+    url: string;
+    options?: any;
 }
 
-export {curl}
+function curl(options: CurlOptions): any {
+
+    if (!options.options) {
+        options.options = {
+            method: "GET"
+        }
+    }
+
+    return new Promise((resolve, reject) => {
+        https.get(options.url, options.options, (response: any) => {
+            let html = '';
+
+            // Receive data in chunks and append to the html variable
+            response.on('data', (chunk: any) => {
+                html += chunk;
+            });
+
+            // Handle the end of the response
+            response.on('end', () => {
+                resolve(html);
+            });
+
+            // Handle errors
+            response.on('error', (error: any) => {
+                reject(error);
+            });
+        }).on('error', (error: any) => {
+            if (config.verbose){
+                reject(error);
+            }else{
+                console.log("ERROR: FAILED TO FETCH "+options.url)
+            }
+        });
+    });
+
+
+}
+
+export {curl, CurlOptions}
